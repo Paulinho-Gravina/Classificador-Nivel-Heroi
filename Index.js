2798ac16e126db5eb643a6b4e8931f720dbec2ad
Paulinho-Gravina <baetagravina@yahoo.com.br>
2025-12-18 13:47:46 -0300

    finalizando o projeto

--- Index.js
+++ Index.js
@@ -1,1 +1,23 @@
-console.log("Olá mundão!");
+let nomeJogador = "Hugo"
+let quantidadeXP = 9030
+let nivelDeHeroi = ""
+
+if(quantidadeXP <= 1000){
+    nivelDeHeroi = "Ferro"
+}else if(quantidadeXP >= 1001 && quantidadeXP < 2000){
+    nivelDeHeroi = "Bronze"
+}else if(quantidadeXP >= 2001 && quantidadeXP < 5000){
+    nivelDeHeroi = "Prata"
+}else if(quantidadeXP >= 5001 && quantidadeXP < 7000){
+    nivelDeHeroi = "Ouro"
+}else if(quantidadeXP >= 7001 && quantidadeXP < 8000){
+    nivelDeHeroi = "Platina"
+}else if(quantidadeXP >= 8001 && quantidadeXP < 9000){
+    nivelDeHeroi = "Ascendente"
+}else if(quantidadeXP >= 9001 && quantidadeXP < 10000){
+    nivelDeHeroi = "Imortal"
+}else{
+    nivelDeHeroi = "Radiante"
+}
+
+console.log("O Herói de nome " + nomeJogador + " está no nível " + nivelDeHeroi)
